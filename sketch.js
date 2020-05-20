8b0cee7ea36ee05ddb067d740e5d1f2dc4a405ff
avisaria <64573387+avisaria@users.noreply.github.com>
2020-05-20 14:59:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(800,400);
+
+  createSprite(75, 250, 50, 300);
+
+  createSprite(162.5, 300, 75, 200);
+
+  createSprite(400, 387.5, 250, 25);
+
+  createSprite(637.5, 300, 75, 200); 
+
+  createSprite(725, 250, 50, 300);
+
+  createSprite(400, 162.5, 550, 25);
+
+  createSprite(400, 50, 700, 50);
+
+
+
+
+
+
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+}
